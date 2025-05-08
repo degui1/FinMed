1e81582b1cdaa60c4cb690034a0f007cc3c91eba
degui1 <gui.denez56@gmail.com>
2025-05-07 23:45:03 -0300

    Add: update simulation controller

--- src/controllers/simulations/update-simulation.controller.ts
+++ src/controllers/simulations/update-simulation.controller.ts
@@ -1,38 +1,43 @@
 import { z } from 'zod'
 import { FastifyReply, FastifyRequest } from 'fastify'
-import { DeleteSimulationService } from '@/services/delete-simulation.service'
+import { UpdateSimulationService } from '@/services/update-simulation.service'
 import { PrismaStudentsRepository } from '@/repositories/prisma/prisma-students.repository'
 import { PrismaFinancingSimulationsRepository } from '@/repositories/prisma/prisma-financing-simulations.repository'
 import { OperationNotPermittedError } from '@/services/erros/operation-not-permitted.error'
 import { ResourceNotFoundError } from '@/services/erros/resource-not-found.error'
+import { InvalidNumberOfInstallmentsError } from '@/services/erros/invalid-number-of-installments.error'
+import { InvalidMonthlyInstallmentAmountError } from '@/services/erros/invalid-monthly-installment-amount.error'
 
-const deleteSimulationBodySchema = z.object({
+const updateSimulationBodySchema = z.object({
   financingSimulationId: z.string().uuid(),
+  installments: z.number().min(1),
+  totalAmountCents: z.number().min(1),
 })
 
-export async function deleteSimulationController(
+export async function updateSimulationController(
   request: FastifyRequest,
   reply: FastifyReply,
 ) {
-  const { financingSimulationId } = deleteSimulationBodySchema.parse(
-    request.body,
-  )
+  const { financingSimulationId, installments, totalAmountCents } =
+    updateSimulationBodySchema.parse(request.body)
 
   try {
     const studentsRepository = new PrismaStudentsRepository()
     const financingSimulationsRepository =
       new PrismaFinancingSimulationsRepository()
-    const deleteSimulationService = new DeleteSimulationService(
+    const updateSimulationService = new UpdateSimulationService(
       studentsRepository,
       financingSimulationsRepository,
     )
 
-    await deleteSimulationService.execute({
+    const { updatedSimulation } = await updateSimulationService.execute({
       studentId: request.user.sub,
       financingSimulationId,
+      installments,
+      totalAmountCents,
     })
 
-    return reply.status(204).send()
+    return reply.status(200).send({ updatedSimulation })
   } catch (error) {
     if (error instanceof ResourceNotFoundError) {
       return reply.status(400).send({ message: error.message })
@@ -40,6 +45,14 @@ export async function deleteSimulationController(
 
     if (error instanceof OperationNotPermittedError) {
       return reply.status(405).send({ message: error.message })
+    }
+
+    if (error instanceof InvalidNumberOfInstallmentsError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    if (error instanceof InvalidMonthlyInstallmentAmountError) {
+      return reply.status(400).send({ message: error.message })
     }
 
     throw error

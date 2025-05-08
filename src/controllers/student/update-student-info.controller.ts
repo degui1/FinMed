d7537f984ecb9c3b42ef2f983debdb605aeb3b37
degui1 <gui.denez56@gmail.com>
2025-05-07 23:22:09 -0300

    Add: Update student info controller

---
+++ src/controllers/student/update-student-info.controller.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { PrismaStudentsRepository } from '@/repositories/prisma/prisma-students.repository'
+import { UpdateStudentInfoService } from '@/services/update-student-info.service'
+import { ResourceNotFoundError } from '@/services/erros/resource-not-found.error'
+
+const updateStudentInfoBodySchema = z.object({
+  name: z.string().optional(),
+  surname: z.string().optional(),
+  password: z.string().min(6).optional(),
+})
+
+export async function updateStudentInfoController(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const { name, password, surname } = updateStudentInfoBodySchema.parse(
+    request.body,
+  )
+
+  try {
+    const studentsRepository = new PrismaStudentsRepository()
+    const updateStudentInfoService = new UpdateStudentInfoService(
+      studentsRepository,
+    )
+
+    await updateStudentInfoService.execute(request.user.sub, {
+      name,
+      password,
+      surname,
+    })
+
+    return reply.status(201).send()
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return reply.status(400).send({ message: error.message })
+    }
+
+    throw error
+  }
+}
